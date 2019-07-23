75e3290336b04355dfce0374af44b7575e5ac3ac
ebenpack <ebenpackwood@gmail.com>
2019-07-23 19:13:27 -0400

    Fix last scraped update

--- src/Lib.hs
+++ src/Lib.hs
@@ -47,7 +47,7 @@ scrapeItCheezy = do
     _ <- processPage `untilM` doneProcessing
     printHits
     processMaybeHits
-    updateLastRunTime
+    saveConfig
     where
     url :: Int -> String
     url pageNum = "https://eztv.ag/" ++ if pageNum == 0 then "" else "page_" ++ show pageNum
@@ -69,8 +69,8 @@ scrapeItCheezy = do
                     Nothing -> if page > end then Just page else Nothing
                 })
     processLink :: Members '[Console, Blacklist, Whitelist, State ProgramConfig] r => Text -> Int -> Link -> Sem r ()
-    processLink previousltLastIndexed page link = do
-        when (href link == previousltLastIndexed) (modify (\s -> s { done = Just page }))
+    processLink previouslyLastIndexed page link = do
+        when (href link == previouslyLastIndexed) (modify (\s -> s { done = Just page }))
         config <- get
         when (isNothing $ lastIndexed config) (modify (\s -> s { lastIndexed = Just $ href link }))
         bl <- getBlacklist $ showName link
@@ -108,7 +108,8 @@ scrapeItCheezy = do
         configEntry' <- getConfigBy configEntry
         lastIndexed' <- getConfigBy lastIndexed
         case (configEntry', lastIndexed') of
-            (Just ce, Just li) -> updateConfig (ce { _configLastIndexedLink = li })
+            (Just ce, Just li) ->
+                updateConfig (ce { _configLastIndexedLink = li })
             _ -> pure ()
         where
         hitDecider :: Members '[Console, Blacklist, Whitelist, State ProgramConfig] r => Text -> Sem r ()
@@ -145,12 +146,13 @@ scrapeItCheezy = do
                 | secs > 3600 = (show (secs `div` 3600) ++ "h") ++ (go (secs `mod` 3600))
                 | secs > 60 = (show (secs `div` 60) ++ "m") ++ (go (secs `mod` 60))
                 | otherwise = show secs ++ "s"
-    updateLastRunTime :: Members '[Time, Config, State ProgramConfig] r => Sem r ()
-    updateLastRunTime = do
+    saveConfig :: Members '[Time, Config, State ProgramConfig] r => Sem r ()
+    saveConfig = do
         t <- getTime
         configEntry' <- getConfigBy configEntry
-        case configEntry' of
-            Just ce -> updateConfig (ce { _configLastIndexedTime = t })
+        lastIndexedUrl <- getConfigBy lastIndexed
+        case (configEntry', lastIndexedUrl) of
+            (Just ce, Just li) -> updateConfig (ce { _configLastIndexedTime = t, _configLastIndexedLink = li })
             _ -> pure ()
     uniqueHits :: Set Link -> Set Text
     uniqueHits = foldr (\link links -> insert (showName link) links) empty
